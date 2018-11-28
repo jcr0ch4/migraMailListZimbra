048c824fefb3eb4a0e8a0c9a1fb0891039152471
Jose Carlos Rocha <jcr0ch4@gmail.com>
2018-11-28 16:03:51 -0200

    Primeiro Commit

---
+++ migraLista.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#  mudar-dominio.sh
+#  
+#  Copyright 2018 Jose Carlos <jcr0ch4@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+# Listas de E-mail
+listas=$(zmprov gadl $1)
+
+# Mensagens
+
+
+for i in $listas
+do
+    # Para cada listya devo listar os membros desta lista e cadastrar na nova lista
+    membros=$(zmprov gdlm $i|grep -v "^$|#|members")
+    listadomain=$(echo $i|awk -F "@" '{print $2}')
+    nomelista=$(echo $i|awk -F "@" '{print $1}')
+    novalistadomain=$nomelista+"@"+$novodmonio
+    for i in $membros
+    do
+# zmprov adlm lista membro
+    done
+
+done 
